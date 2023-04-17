4e8e6d313093c55245095bffe946e1a49a825081
Tanaya <desaitanaya17@gmail.com>
2023-04-17 12:59:50 +0530

    [Tanaya] Add : Java Program to find the Maximum among Float values in array using compareTo method.

--- Find_Maximum.java
+++ Find_Maximum.java
@@ -13,9 +13,33 @@ public class Find_Maximum {
 		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
 		for (int i = 1; i < inputArray.length; i++) {
 			/*
+			 * Using compareTo method to find maximum among integer values in array
+			 */
+			if ((inputArray[i].compareTo(max)) > 0) {
+				max = inputArray[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println("---------------------------------------------------------");
+	}
+
+	public static void toPrintMaxDouble(Double[] inputArray) {
+		System.out.println("ELEMENTS IN ARRAY : ");
+		for (double element : inputArray) {
+			System.out.printf("%s", element);
+			System.out.println();
+		}
+		System.out.println();
+
+		int x;
+		double max = inputArray[0];
+		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
+		for (int i = 1; i < inputArray.length; i++) {
+			/*
 			 * Using compareTo method 
-			 * to find maximum among 
-			 * integer values in array
+			 * to find maximum 
+			 * among float values 
+			 * in array
 			 */
 			if ((inputArray[i].compareTo(max)) > 0) {
 				max = inputArray[i];
@@ -31,8 +55,11 @@ public class Find_Maximum {
 
 		// Integer array
 		Integer[] intarr = { 10, 35, 41, 2, 6 };
-
 		Find_Maximum.toPrintMaxInteger(intarr);
+
+		//Double array
+		Double[] doublearr = { 12.4, 54.3, 64.0, 2.4, 25.7 };
+		Find_Maximum.toPrintMaxDouble(doublearr);
 	}
 
 }
