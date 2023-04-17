bb0af651245e471e5a97bbf4f7c6094684f96825
Tanaya <desaitanaya17@gmail.com>
2023-04-17 12:45:23 +0530

    [Tanaya] Add : Java Program to find the Maximum among Integer values in array using compareTo method.

---
+++ Find_Maximum.java
@@ -0,0 +1,38 @@
+public class Find_Maximum {
+
+	public static void toPrintMaxInteger(Integer[] inputArray) {
+		System.out.println("ELEMENTS IN ARRAY : ");
+		for (int element : inputArray) {
+			System.out.printf("%s", element);
+			System.out.println();
+		}
+		System.out.println();
+
+		int x;
+		int max = inputArray[0];
+		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
+		for (int i = 1; i < inputArray.length; i++) {
+			/*
+			 * Using compareTo method 
+			 * to find maximum among 
+			 * integer values in array
+			 */
+			if ((inputArray[i].compareTo(max)) > 0) {
+				max = inputArray[i];
+			}
+		}
+		System.out.println(max);
+	}
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("WELCOME TO FIND MAXIMUM PROBLEM USING GENERICS PROGRAM");
+		System.out.println("---------------------------------------------------------");
+
+		// Integer array
+		Integer[] intarr = { 10, 35, 41, 2, 6 };
+
+		Find_Maximum.toPrintMaxInteger(intarr);
+	}
+
+}
