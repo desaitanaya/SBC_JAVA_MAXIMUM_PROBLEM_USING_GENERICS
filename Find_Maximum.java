c64868f59bb3b37d2191117f7c5d72da4e25b257
Tanaya <desaitanaya17@gmail.com>
2023-04-18 17:07:59 +0530

    [Tanaya] Add : Java Program to find the Maximum values in array using Generics.

--- Find_Maximum.java
+++ Find_Maximum.java
@@ -1,80 +1,39 @@
 public class Find_Maximum {
 
-	public static void toPrintMaxInteger(Integer[] inputArray) {
+	/*
+	 * Implementing maximum problem 
+	 * using Generics
+	 */
+	public static <E extends Comparable<E>> E toPrintMaxGenerics(E[] inputArray) {
 		System.out.println("ELEMENTS IN ARRAY : ");
-		for (int element : inputArray) {
+		
+		/*Displaying the elements
+		 * in array
+		 */
+		for (E element : inputArray) {
 			System.out.printf("%s", element);
 			System.out.println();
 		}
 		System.out.println();
 
-		int x;
-		int max = inputArray[0];
+		
+		E max = inputArray[0];
 		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
 		for (int i = 1; i < inputArray.length; i++) {
 			/*
 			 * Using compareTo method 
-			 * to find maximum among 
-			 * integer values in array
+			 * to find maximum element 
+			 * in array
 			 */
-			if ((inputArray[i].compareTo(max)) > 0) {
+			if (inputArray[i].compareTo(max) > 0) {
 				max = inputArray[i];
 			}
 		}
 		System.out.println(max);
 		System.out.println("---------------------------------------------------------");
+		return max;
+
 	}
-	
-
-	public static void toPrintMaxDouble(Double[] inputArray) {
-		System.out.println("ELEMENTS IN ARRAY : ");
-		for (double element : inputArray) {
-			System.out.printf("%s", element);
-			System.out.println();
-		}
-		System.out.println();
-
-		int x;
-		double max = inputArray[0];
-		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
-		for (int i = 1; i < inputArray.length; i++) {
-			/*
-			 * Using compareTo method 
-			 * to find maximum among 
-			 * float values in array
-			 */
-			if ((inputArray[i].compareTo(max)) > 0) {
-				max = inputArray[i];
-			}
-		}
-		System.out.println(max);
-		System.out.println("---------------------------------------------------------");
-	}
-	
-
-	public static void toPrintMaxString(String[] inputArray) {
-		System.out.println("ELEMENTS IN ARRAY : ");
-		for (String element : inputArray) {
-			System.out.printf("%s", element);
-			System.out.println();
-		}
-		System.out.println();
-
-		String max = inputArray[0];
-		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
-		for (int i = 1; i < inputArray.length; i++) {
-			/*
-			 * Using compareTo method 
-			 * to find maximum among 
-			 * string values in array
-			 */
-			if ((inputArray[i].compareTo(max)) > 0) {
-				max = inputArray[i];
-			}
-		}
-		System.out.println(max);
-	}
-	
 
 	public static void main(String[] args) {
 		System.out.println();
@@ -83,15 +42,15 @@ public class Find_Maximum {
 
 		// Integer array
 		Integer[] intarr = { 10, 35, 41, 2, 6 };
-		Find_Maximum.toPrintMaxInteger(intarr);
+		Find_Maximum.toPrintMaxGenerics(intarr);
 
 		// Double array
 		Double[] doublearr = { 12.4, 54.3, 64.0, 2.4, 25.7 };
-		Find_Maximum.toPrintMaxDouble(doublearr);
+		Find_Maximum.toPrintMaxGenerics(doublearr);
 
 		// String array
 		String[] stringarr = { "Apple", "Mango", "Banana", "Orange", "Pineapple" };
-		Find_Maximum.toPrintMaxString(stringarr);
+		Find_Maximum.toPrintMaxGenerics(stringarr);
 	}
 
 }
